3271d9799d7610f113fc1d62f489341989baa1e5
Heidi Mork <heidimork@yahoo.no>
2025-02-06 20:39:41 +0100

    mer data

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Slekt
+
+## Todo
+
+* Konsistenssjekk på lenking av data
+* Styling
+* Komponenter
+* Deploy til github pages
--- astro/src/content.config.ts
+++ astro/src/content.config.ts
@@ -4,14 +4,15 @@ import { glob } from 'astro/loaders';
 const hendelser = defineCollection({
   loader: glob({ pattern: '**/[^_]*.yml', base: "./../hendelser" }),
   schema: z.object({
-    type: z.enum(['dåp', 'konfirmasjon', 'bryllup']),
+    type: z.enum(['dåp', 'konfirmasjon', 'vielse']),
     dato: z.string().date(),
     sted: z.string(),
     personer: z.array(reference('personer')),
     kilde: z.object({
       tittel: z.string(),
       url: z.string()
-    })
+    }).optional(),
+    kommentar: z.string().optional()
   })
 });
 
@@ -22,9 +23,11 @@ const personer = defineCollection({
     etternavn: z.string(),
     pikenavn: z.optional(z.string()),
     født: z.string().date(),
+    beskrivelse: z.string().optional(),
     hendelser: z.optional(z.array(reference('hendelser'))),
     mor: z.optional(reference('personer')),
     far: z.optional(reference('personer')),
+    barn: z.optional(z.array(reference('personer')))
   })
 });
 
