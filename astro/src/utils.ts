0ae852c40c87f71da774002e6eb4154a65773e5f
Heidi Mork <heidimork@yahoo.no>
2025-02-16 13:18:12 +0100

    lagt til barn og ektefelle i hendelse for person

--- astro/src/utils.ts
+++ astro/src/utils.ts
@@ -1,4 +1,7 @@
+import type { Person } from "./types"
 
 export const upperFirstChar = (str: string) => str[0].toUpperCase() + str.slice(1)
 
 export const formatDate = (date: string) => new Date(date).toLocaleDateString("no-NB")
+
+export const getPersonDisplayName = (person: Person) => `${person.fornavn} ${person.etternavn}`
