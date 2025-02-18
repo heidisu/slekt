66fa29f13cb78c4fa5a891e07bb85128b3804b5e
Heidi Mork <heidimork@yahoo.no>
2025-02-18 21:48:41 +0100

    add tests

---
+++ astro/verify-model.test.ts
@@ -0,0 +1,29 @@
+import { getCollection } from "astro:content"
+import { describe, expect, test } from "vitest"
+
+const personer = await getCollection("personer")
+const hendelser = await getCollection("hendelser")
+
+describe.each(personer)("$id", ({ id, data }) => {
+    test(`person er referert i egne hendelser`, () => {
+            data.hendelser?.forEach((hendelse) => {
+                console.log(id, hendelse)
+            const eksisterendeHendelse = hendelser.find((h) => h.id == hendelse.id)
+            expect(eksisterendeHendelse).toBeDefined()
+            console.log(eksisterendeHendelse?.data.personer)
+            expect(eksisterendeHendelse?.data.personer.map((p) => p.id)).contains(id)
+            })
+        }
+    )
+})
+
+describe.each(hendelser)("$id", ({ id, data }) => {
+    test(`hendelse er referert hos alle personer`, () => {
+            data.personer.forEach((person) => {
+            const eksisterendePerson = personer.find((p) => p.id == person.id)
+            expect(eksisterendePerson).toBeDefined()
+            expect(eksisterendePerson?.data.hendelser?.map((h) => h.id)).contains(id)
+            })
+        }
+    )
+})
